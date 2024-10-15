da208989891a514d92210262bc9799eec6159b98
TUF GAMING <TUF GAMING@192.168.31.49>
2024-10-15 22:23:06 +0530

    Initial Commit

---
+++ Servlets/src/main/java/com/servleteg1/FirstServlet.java
@@ -0,0 +1,68 @@
+package com.servleteg1;
+//Run as- Run on Server- http://localhost:5786/Servlets/FirstServlet
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet implements Servlet {
+
+	/**
+	 * Default constructor.
+	 */
+	public FirstServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see Servlet#getServletInfo()
+	 */
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		// general settings - common for all applications
+		response.setContentType("text/html");
+
+		PrintWriter writer = response.getWriter();
+		writer.print("This is First Servlets Application created by Er.Sajid Patel ");
+
+	}
+
+}
